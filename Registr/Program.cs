59d4526044847f41867c4b803b6fa4999405fcc1
dadsds <olexandrvoychishin@mail.ru>
2015-11-06 20:48:15 +0200

    Init

---
+++ Registr/Program.cs
@@ -0,0 +1,77 @@
+﻿// Реєстраційна форма. Створити клас RegistrationForm, в цьому класі створити властивості Password (string), Login (string), Capcha (string).
+// Створити приватні поля trueLogin, truePassword (strings). Ці поля заповнюються в коді.
+// Пароль має складатись з 8 символів, серед них букви нижнього регістру, верхнього регістру, цифри (PassWord123).
+// Якщо задаємо пароль, що не містить чогось з переліченого вище - клас викидає виключення.
+// Логін може бути будь-яким, але починатись має не на цифри.
+// Капчу можна згенерувати за допомогою System.Random
+// Інтерфейс: запитати у користувача логін. Якщо його введений логін вже не співпадає з умовами задання логіну (згенерувалось виключення)
+// то видається повідомлення і робота завершується
+// Якщо логін введено за шаблоном, запитати пароль. Якщо пароль введено за шаблоном - згенерувати і запитати капчу. Якщо користувач все ввів вірно
+// вивести відповідне повідомлення
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registr
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistrationForm admin = new RegistrationForm();
+            admin.Login = "Admin";
+            admin.Password = "Admin1234";
+            for (;;)
+            {
+                RegistrationForm form = new RegistrationForm();
+                Console.WriteLine("Enter login:");
+                try
+                {
+                    form.Login = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+
+                Console.WriteLine("Enter password:");
+
+                try
+                {
+                    form.Password = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+
+                string capcha = form.Capcha;
+                Console.WriteLine("Enter capcha:");
+                Console.WriteLine(capcha);
+                while (capcha != Console.ReadLine())
+                {
+                    Console.WriteLine("Wrong capcha input");
+                    capcha = form.Capcha;
+                    Console.WriteLine(capcha);
+                }
+                if (admin.Login != form.Login || admin.Password != form.Password)
+                {
+                    Console.WriteLine("Autentification declined");
+                }
+                else
+                {
+                    Console.WriteLine("Autentification accept");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Registr/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Registr")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Registr")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("bc4f6139-6d1c-4351-9d7a-02cd6cfd1d8d")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
